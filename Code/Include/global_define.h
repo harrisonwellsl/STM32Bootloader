db742e31b9d5abced6122dac96b0d4933454ff67
harrison <harrisonwellsl@outlook.com>
2020-09-21 22:18:51 +0800

    更改置位和清除位的实现方式

--- Code/Include/global_define.h
+++ Code/Include/global_define.h
@@ -1,9 +1,20 @@
 #ifndef __GLOBAL_DEFINE_H__
 #define __GLOBAL_DEFINE_H__
 
-#define             STATUS              int
-#define             OK                  (0)
-#define             ERROR               (-1)
+#include <stdint.h>
+
+#define             STATUS                                  int
+#define             OK                                      (0)
+#define             ERROR                                   (-1)
+
+/* 置位宏定义 */
+#define             BIT_VAL_SET(reg_addr, p_val)            do { \
+                                                                *(uint32_t *)reg_addr = *(uint32_t *)p_val | *(uint32_t *)reg_addr; \
+                                                            }while(0)
+/* 清除位宏定义 */
+#define             BIT_VAL_RESET(reg_addr, p_val)          do { \
+                                                                *(uint32_t *)reg_addr = ~(*(uint32_t *)p_val) & (*(uint32_t *)reg_addr); \
+                                                            }while(0)
 
 #endif /* __GLOBAL_DEFINE_H__ */
 

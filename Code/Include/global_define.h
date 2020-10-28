b65272d85a31611802721be35d5ee5ee85b8ef4a
harrison <harrisonwellsl@outlook.com>
2020-10-28 23:18:47 +0800

    从头来过

--- Code/Include/global_define.h
+++ Code/Include/global_define.h
@@ -7,14 +7,6 @@
 #define             OK                                      (0)
 #define             ERROR                                   (-1)
 
-/* 置位宏定义 */
-#define             BIT_VAL_SET(reg_addr, p_val)            do { \
-                                                                *(uint32_t *)reg_addr = *(uint32_t *)p_val | *(uint32_t *)reg_addr; \
-                                                            }while(0)
-/* 清除位宏定义 */
-#define             BIT_VAL_RESET(reg_addr, p_val)          do { \
-                                                                *(uint32_t *)reg_addr = ~(*(uint32_t *)p_val) & (*(uint32_t *)reg_addr); \
-                                                            }while(0)
 
 #endif /* __GLOBAL_DEFINE_H__ */
 

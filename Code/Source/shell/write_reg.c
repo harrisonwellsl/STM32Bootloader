eb8164693f15fa095173b80d581e60224e7f6131
harrison <harrisonwellsl@outlook.com>
2020-11-09 22:37:38 +0800

    修改写寄存器函数

--- Code/Source/shell/write_reg.c
+++ Code/Source/shell/write_reg.c
@@ -4,11 +4,8 @@ int write_reg(uint32_t arg_addr) {
     char in_args[5][32] = {0};
     memcpy(in_args, (void *)arg_addr, 5 * 32);
     uint32_t addr = strtoul(in_args[1], 0, 0);
-    stm32UsartPrintf("\r\n0x%x", addr);
     uint32_t value = strtoul(in_args[2], 0, 0);
-    stm32UsartPrintf("\r\n0x%x", value);
     *(uint32_t *)addr = value;
-    stm32UsartPrintf("\r\n0x%x", *(uint32_t *)addr);
     return value;
 }
 
